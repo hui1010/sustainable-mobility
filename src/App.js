0d7f9b7a59c88b48527cc097472a50a78f9cccdd
Huiyi Skårner <huiyi.skarner@ist.com>
2022-04-13 23:05:21 +0200

    Minor changes on style and content

--- src/App.js
+++ src/App.js
@@ -10,8 +10,10 @@ function App() {
           The final interpretation right of this event belongs to IST
           Sustainable Expert Group
         </p>
-        <p> &#129505;Ackowledge to Richard Lundby for the design support</p>
-        <p>&copy; 2022 Huiyi Skårner</p>
+        <p>
+          &#129505;Ackowledge to Richard Lundby for the design
+          support&emsp;&emsp;&emsp; &copy; 2022 Huiyi Skårner
+        </p>
       </div>
     </div>
   );
